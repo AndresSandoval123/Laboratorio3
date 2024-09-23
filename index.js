c4ed7b80e2cd5ed3556683c90ea5aba97a96cf91
Andres.Sandoval123 <andres.sandoval@genstudents.org>
2024-09-23 10:53:05 -0500

    proyecto okey

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const cors = require("cors");
 const contrPag = require("./controlador/contrPag"); // Importo el controlador
 
 const port = 3000;
@@ -8,20 +9,23 @@ const hostname = "localhost"
 
 const app = express(); // Con esto le decimos que traiga todas las caracteristicas de express para poder usarla.
 
-app.use(express.static(path.join(__dirname, `vista/public`)));//Establece la ruta de donde express debe tomar los archivos.
-app.use(express.static(path.join(__dirname, `vista/styles`)));//Establece la ruta de donde express debe tomar los archivos.
-app.use(express.static(path.join(__dirname, `vista/script`)));//Establece la ruta de donde express debe tomar los archivos.
+app.use(cors()); // Permite todas las solicitudes CORS
 
-// Ruta para manejar la solicutud a las páginas.
-app.get(`/pagina/:pagina`, contrPag.mostrarPagina);
+app.use(express.static(path.join(__dirname, `vista`)));//Establece la ruta de donde express debe tomar los archivos.
+
+// Ruta para manejar la solicitud a las páginas.
+app.get(`/:pagina`, (req, res) => {
+  const pagina = req.params.pagina;
+  res.sendFile(path.join(__dirname, 'vista/public', pagina)); // Sirve el archivo correspondiente
+});
 
 app.get("/",(req,res)=>{
-  res.sendFile(path.join(__dirname, "index.html")); //Se establece el index como muestra al principio.
+  res.sendFile(path.join(__dirname, "/vista/public/index.html")); //Se establece el index como muestra al principio.
 });
 
 // Se crea el servidor.
 const server = http.createServer(app);
 // Para que funcione o escuche nuestro servidor
 server.listen(port, hostname, () => {
-  console.log(`servidor en ejecución en http://${hostname}${port}`);
+  console.log(`servidor en ejecución en http://${hostname}:${port}`);
 });
--- vista/script/pag.js
+++ vista/script/pag.js
@@ -1,10 +1,44 @@
-document.getElementById('verPaginaBtn').addEventListener('click', function() {
-    const paginaSeleccionada = document.getElementById('paginaSelect').value;
-    const iframe = document.getElementById('iframeContent');
+const verPaginaBtn = document.getElementById("verPaginaBtn");
+const iframeContent = document.getElementById('iframeContent');
 
-    if (paginaSeleccionada) {
-        iframe.src = `/pagina/${paginaSeleccionada}`;
-    } else {
-        alert("Por favor, selecciona una página.");
-    }
+
+//...............................>
+// Evento para ver la información
+//...............................>
+
+verPaginaBtn.addEventListener("click", () => {
+  let paginaSeleccionada = document.getElementById('paginaSelect').value;
+  let metodoSelect = document.getElementById("metodoSelect").value;
+
+  if (paginaSeleccionada == "") {
+    alert(" Selecciona una página, por favor");
+    return; // Salé de la condición.
+  }
+
+  //...............................>
+  // Conexión Back-end
+  //...............................>
+
+  const url = `http://localhost:3000/${paginaSeleccionada}`
+
+  if (metodoSelect == "get") {
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          console.log(`Conexión exitosa con el back-end que esta en la url ${url}`);
+        } else {
+          throw new Error("Error de conexión");
+        }
+        return response.text(); // Convierte la respuesta a texto (HTML)
+      })
+      .then(html => {
+        iframeContent.srcdoc = html
+      })
+      .catch(error => {
+        //Manejo de error
+        console.log(`Hay un error con la petición ${error}`);
+      });
+  } else {
+    alert(`Solo esta permitido consultar con el metodo "GET"`)
+  }
 });
